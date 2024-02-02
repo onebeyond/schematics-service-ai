ec897e2617bb2b4985799bdf5f82f1af9cadeec2
Daniel Alarcon <daniel.alarcon@onebeyond.com>
2024-02-02 13:04:27 +0100

    feat: basic dev stuff

--- src/schematics-service-ai/index_spec.ts
+++ src/schematics-service-ai/index_spec.ts
@@ -1,16 +1,17 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import {Tree} from '@angular-devkit/schematics';
+import {SchematicTestRunner} from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('schematics-service-ai', () => {
-  it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematic('schematics-service-ai', {
-      serviceName: 'test-service',
-    }, Tree.empty());
+    it('works', async () => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
+        const tree = await runner.runSchematic('schematics-service-ai', {
+            serviceName: 'test-service',
+            destinationPath: 'test-dist',
+        }, Tree.empty());
 
-    expect(tree.files).toEqual([]);
-  });
+        expect(tree.files.length).toEqual(33);
+    });
 });
